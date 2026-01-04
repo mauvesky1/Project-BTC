d548064478070ad0b3c121ef4124192bd4aed54b
RobertDraper <rob.draper@live.com>
2026-01-04 17:47:55 +0000

    Add basic header component to landing page

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+      <header className="px-4 py-3 border-b border-neutral-800">
+        <h1 className="text-lg font-semibold">
+          MyTube
+        </h1>
+      </header>
+    );
+  }
+  
